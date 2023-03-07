695fdc1c232869fcafc109991038db29d35c9eb5
Damian-luis <damian.luis.porta@gmail.com>
2023-03-07 08:13:07 -0300

    bug: debugging2

--- index.js
+++ index.js
@@ -10,7 +10,9 @@ const conexionDb=require('./db/index')
 require('dotenv').config()
 const PORT=process.env.PORT || 3000
 app.use(express.json())
-app.use(cors())
+app.use(cors({
+  origin: '*'
+}))
 const fileUpload=require("express-fileUpload")
 app.use(fileUpload({
     useTempFiles : true,
