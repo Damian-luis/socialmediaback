53dc41429033db4ad91847893d08a792dc04cec5
Damian-luis <damian.luis.porta@gmail.com>
2023-03-17 14:41:27 -0300

    env fiels changed

--- controllers/users.controller.js
+++ controllers/users.controller.js
@@ -10,7 +10,7 @@ const AWS=require('aws-sdk')
 const s3=new AWS.S3({
   region:process.env.REGION,
   accessKeyId:process.env.ACCESS_KEY_ID,
-  secretAccessKey:SECRET_ACESS_KEY_ID
+  secretAccessKey:process.env.SECRET_ACESS_KEY_ID
 })
 const fs=require('fs')
 module.exports={
