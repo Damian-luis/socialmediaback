ff43d1e2626f87450dea53a214196c4259161b71
damianduran1-admin <damian.luis.porta@gmail.com>
2024-02-14 16:58:41 -0300

    refactorint getUserData controller:added url profile of commenters

--- controllers/users.controller.js
+++ controllers/users.controller.js
@@ -28,50 +28,79 @@ module.exports={
          message:"Usuarios enviados con exito"
       })
     },
-    getUser:async(req, res) =>{
-        //const data=await User.doc("vuXrg8qfehSBCmvsfIY8").update({name:"el sexpotae"})
-        const {id}=req.params
-try{
-        //Data basica
-        const data=await User.get()
-      const response=await data.docs.map(e=>{return {
-         name:e._fieldsProto.name.stringValue,
-         lastname:e._fieldsProto.lastname.stringValue,
-         mail:e._fieldsProto.mail.stringValue,
-         date:e._fieldsProto.date.stringValue,
-         time:e._fieldsProto.time.stringValue,
-         urlProfile:e._fieldsProto.urlProfile.stringValue,
-         id:e._ref._path.segments[1]
-      }})
-      const dataBasica=response.filter(e=>{return e.id===id}) 
-
-      //Post del usuario seleccionado
-      const posts=await Post.get()
-      const publicaciones=posts.docs.map(e=>{return {
-         publicacion:e._fieldsProto.publicacion.stringValue,
-         nombre:e._fieldsProto.name.stringValue,
-         apellido:e._fieldsProto.lastname.stringValue,
-         idUser:e._fieldsProto.idUser.stringValue,
-         date:e._fieldsProto.date.stringValue,
-         time:e._fieldsProto.time.stringValue,
-         idPublicacion:e._ref._path.segments[1]
-     }})
-     const publicacionesUser=publicaciones.filter(e=>{return e.idUser===id})
-     res.status(200).json({
-      status:true,
-      message:"Informacion recuperada exitosamente",
-      dataBasica,
-      publicacionesUser
-     })}
-     catch(e){
-      res.status(400).json({
-         status:false,
-         message:"No se ha podido acceder al perfil"
-      })
-     }
-
-     
-    },
+    getUser: async (req, res) => {
+      const { id } = req.params;
+  console.log("llega al controlador ")
+      try {
+          // Datos básicos del usuario
+          const userData = await User.doc(id).get();
+          const userDataObj = {
+              name: userData._fieldsProto.name.stringValue,
+              lastname: userData._fieldsProto.lastname.stringValue,
+              mail: userData._fieldsProto.mail.stringValue,
+              date: userData._fieldsProto.date.stringValue,
+              time: userData._fieldsProto.time.stringValue,
+              urlProfile: userData._fieldsProto.urlProfile.stringValue,
+              id: userData._ref._path.segments[1],
+          };
+  
+          console.log("Datos básicos del usuario:", userDataObj);
+  
+          // Obtener publicaciones del usuario
+          const postsData = await Post.get();
+          const userPosts = postsData.docs.map((doc) => ({
+              publicacion: doc._fieldsProto.publicacion.stringValue,
+              nombre: doc._fieldsProto.name.stringValue,
+              apellido: doc._fieldsProto.lastname.stringValue,
+              idUser: doc._fieldsProto.idUser.stringValue,
+              date: doc._fieldsProto.date.stringValue,
+              time: doc._fieldsProto.time.stringValue,
+              idPublicacion: doc._ref._path.segments[1],
+              usersComments: doc._fieldsProto.usersComments.arrayValue.values.map((comment) => ({
+                  name: comment.mapValue.fields.name.stringValue,
+                  lastname: comment.mapValue.fields.lastname.stringValue,
+                  time: comment.mapValue.fields.time.stringValue,
+                  date: comment.mapValue.fields.date.stringValue,
+                  comment: comment.mapValue.fields.comment.stringValue,
+                  idComment: comment.mapValue.fields.idComment.stringValue,
+                  idUser: comment.mapValue.fields.idUser.stringValue,
+                  urlProfile: "",
+              })),
+          }));
+  
+          console.log("Publicaciones del usuario:", userPosts);
+  
+          
+          for (let i = 0; i < userPosts.length; i++) {
+              for (let j = 0; j < userPosts[i].usersComments.length; j++) {
+                  const commenterId = userPosts[i].usersComments[j].idUser;
+                  const commenterData = await User.doc(commenterId).get();
+                  userPosts[i].usersComments[j].urlProfile = commenterData._fieldsProto.urlProfile.stringValue;
+              }
+          }
+  
+          console.log("Publicaciones con URL de perfil:", userPosts);
+  
+          res.status(200).json({
+              status: true,
+              message: "Informacion recuperada exitosamente",
+              user: [userDataObj],
+              post: userPosts,
+          });
+      } catch (error) {
+          console.error(error);
+          res.status(400).json({
+              status: false,
+              message: "No se ha podido acceder al perfil",
+          });
+      }
+  },
+  
+  
+  
+  
+  
+  
     getLogin:async (req, res) =>{
       const {mail,password}=req.body
       const data=await User.get() 
@@ -123,78 +152,82 @@ try{
       
        
     },
-    getUserData:async (req, res) =>{
-      const {id}=req.params
-
-      try{
-         const data=await User.get()
-      const response=await data.docs.map(e=>{return {
-         name:e._fieldsProto.name.stringValue,
-         lastname:e._fieldsProto.lastname.stringValue,
-         mail:e._fieldsProto.mail.stringValue,
-         date:e._fieldsProto.date.stringValue,
-         time:e._fieldsProto.time.stringValue,
-         country:e._fieldsProto.country.stringValue,
-         liveCountry:e._fieldsProto.liveCountry.stringValue,
-         ocupation:e._fieldsProto.ocupation.stringValue,
-         birthday:e._fieldsProto.birthday.stringValue,
-         urlProfile:e._fieldsProto.urlProfile.stringValue,
-         id:e._ref._path.segments[1]
-      }})
-      const dataBasica=response.filter(e=>{return e.id===id})
- 
-      //Post del usuario seleccionado
-      const posts=await Post.get()
-      
-            const publicaciones=posts.docs.map(e=>{return {
-                publicacion:e._fieldsProto.publicacion.stringValue,
-                nombre:e._fieldsProto.name.stringValue,
-                apellido:e._fieldsProto.lastname.stringValue,
-                idUser:e._fieldsProto.idUser.stringValue,
-                date:e._fieldsProto.date.stringValue,
-                time:e._fieldsProto.time.stringValue,
-                usersComments:e._fieldsProto.usersComments.arrayValue.values.map(e=>{return {
-                    name:e.mapValue.fields.name.stringValue,
-                    lastname:e.mapValue.fields.lastname.stringValue,
-                    time:e.mapValue.fields.time.stringValue,
-                    date:e.mapValue.fields.date.stringValue,
-                    comment:e.mapValue.fields.comment.stringValue,
-                    idComment:e.mapValue.fields.idComment.stringValue,
-                    idUser:e.mapValue.fields.idUser.stringValue
-                }}),
-                usersLinked:e._fieldsProto.usersLinked.arrayValue.values.map(e=>{return {
-                    name:e.mapValue.fields.name.stringValue,
-                    lastname:e.mapValue.fields.lastname.stringValue,
-                    idLike:e.mapValue.fields.idLike.stringValue,
-                    idUser:e.mapValue.fields.idUser.stringValue
-                }}),
-                idPublicacion:e._ref._path.segments[1]
-            }})
-
-        
-            
-        
-         const misPublicaciones=publicaciones.filter(e=>{return e.idUser===id})
-         
-         res.status(200).json({
-            status:true,
-            message:"Informacion recuperada exitosamente",
-            user:dataBasica,
-            post:misPublicaciones
-         })
+    getUserData: async (req, res) => {
+      const { id } = req.params;
+  
+      try {
+          // Obtener datos básicos del usuario
+          const userData = await User.get();
+          const response = userData.docs.map(e => ({
+              name: e._fieldsProto.name.stringValue,
+              lastname: e._fieldsProto.lastname.stringValue,
+              mail: e._fieldsProto.mail.stringValue,
+              date: e._fieldsProto.date.stringValue,
+              time: e._fieldsProto.time.stringValue,
+              country: e._fieldsProto.country.stringValue,
+              liveCountry: e._fieldsProto.liveCountry.stringValue,
+              ocupation: e._fieldsProto.ocupation.stringValue,
+              birthday: e._fieldsProto.birthday.stringValue,
+              urlProfile: e._fieldsProto.urlProfile.stringValue,
+              id: e._ref._path.segments[1],
+          }));
+  
+          const userDataBasica = response.filter(e => e.id === id);
+  
+          // Obtener publicaciones del usuario seleccionado
+          const postsData = await Post.get();
+          const publicaciones = postsData.docs.map(e => ({
+              publicacion: e._fieldsProto.publicacion.stringValue,
+              nombre: e._fieldsProto.name.stringValue,
+              apellido: e._fieldsProto.lastname.stringValue,
+              idUser: e._fieldsProto.idUser.stringValue,
+              date: e._fieldsProto.date.stringValue,
+              time: e._fieldsProto.time.stringValue,
+              usersComments: e._fieldsProto.usersComments.arrayValue.values.map(comment => {
+                  const commentData = comment.mapValue.fields;
+                  const commentUser = response.find(user => user.id === commentData.idUser.stringValue);
+                  return {
+                      name: commentUser.name,
+                      lastname: commentUser.lastname,
+                      time: commentData.time.stringValue,
+                      date: commentData.date.stringValue,
+                      comment: commentData.comment.stringValue,
+                      idComment: commentData.idComment.stringValue,
+                      idUser: commentData.idUser.stringValue,
+                      urlProfile: commentUser.urlProfile, // Agregar la URL del perfil
+                  };
+              }),
+              usersLinked: e._fieldsProto.usersLinked.arrayValue.values.map(linkedUser => {
+                  const linkedUserData = linkedUser.mapValue.fields;
+                  const linkedUserDetail = response.find(user => user.id === linkedUserData.idUser.stringValue);
+                  return {
+                      name: linkedUserDetail.name,
+                      lastname: linkedUserDetail.lastname,
+                      idLike: linkedUserData.idLike.stringValue,
+                      idUser: linkedUserData.idUser.stringValue,
+                      urlProfile: linkedUserDetail.urlProfile, // Agregar la URL del perfil
+                  };
+              }),
+              idPublicacion: e._ref._path.segments[1],
+          }));
+  
+          const misPublicaciones = publicaciones.filter(e => e.idUser === id);
+  
+          res.status(200).json({
+              status: true,
+              message: "Informacion recuperada exitosamente",
+              user: userDataBasica,
+              post: misPublicaciones,
+          });
+      } catch (error) {
+          console.log(error);
+          res.status(400).json({
+              status: false,
+              message: "No se ha podido recuperar la informacion del usuario",
+          });
       }
-      catch(e){
-         console.log(e)
-         res.status(400).json({
-            status:false,
-            message:"No se ha podido recuperar la informacion del usuario"
-         })
-      }
-      
-      
-      
-       
-    },
+  },
+  
     getLogout:(req, res) =>{
         const authHeader = req.headers["authorization"];
    jwt.sign(authHeader, "", { expiresIn: '1s' } , (logout, err) => {
